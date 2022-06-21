bbca3b067cabc986b802b6608ca4fe8a2f7c88cb
Ly Van Bong <87922239+LyVanBong@users.noreply.github.com>
2022-06-22 00:21:57 +0700

    Thêm dự an phát triển phầm mềm zalotool

---
+++ ZaloTool/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using System.Windows;
+using ZaloTool.Views;
+
+namespace ZaloTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
